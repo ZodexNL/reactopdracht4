0c3818570d0a4030d4576633ac35352cc30e89f5
Daniel <zodexnl@gmail.com>
2022-04-03 17:20:55 +0200

    ja

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
-function App() {
+import "./App.css";
+
+const useFetch = url => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  // Similar to componentDidMount and componentDidUpdate:
+  useEffect(async () => {
+    const response = await fetch(url);
+    const data = await response.json();
+    const [item] = data.results;
+    setData(item);
+    setLoading(false);
+  }, []);
+
+  return { data, loading };
+};
+
+export default () => {
+
+  const { data, loading } = useFetch("https://api.randomuser.me/");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+     
+
+    <div>
+       <h1>Ik maak gebruik van de Random User API</h1>
+      <table>
+        <tr>
+          <th>Naam:</th>
+          <th>Gender:</th>
+          <th>Email:</th>
+          <th>Adres:</th>
+          <th>Timezone:</th>
+          <th>Geboortedatum:</th>
+          <th>Telefoon:</th>
+        </tr>
+        <tr>
+          <td>{loading ? <div>...loading</div> : <div>{data.name.title} {data.name.first} {data.name.last}</div>}</td>
+          <td>{loading ? <div>...loading</div> : <div>{data.gender}</div>}</td>
+          <td>{loading ? <div>...loading</div> : <div>{data.email}</div>}</td>
+          <td>{loading ? <div>...loading</div> : <div>{data.location.street.number} {data.location.street.name}, {data.location.city}, {data.location.country}</div>}</td>
+          <td>{loading ? <div>...loading</div> : <div>{data.location.timezone.description}, {data.location.timezone.offset}</div>}</td>
+          <td>{loading ? <div>...loading</div> : <div>{data.dob.date}, {data.dob.age}</div>}</td>
+          <td>{loading ? <div>...loading</div> : <div>{data.phone}</div>}</td>
+        </tr>
+      </table>
+    </div>
+
+  );
+};
+
+
